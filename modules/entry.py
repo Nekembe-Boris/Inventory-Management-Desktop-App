28941fc2c303a1fb3d8406b39a2b79fb41037e51
nekembe <nekembeb@gmail.com>
2024-01-28 15:43:38 +0100

    creating a module directory and readjusting the file paths

---
+++ modules/entry.py
@@ -0,0 +1,219 @@
+"""This module is responsible for all the functionalities of the ENTRY TAB"""
+
+from tkinter import Frame, END
+from tkinter import messagebox
+import datetime
+import pandas
+from modules.functions import clear, forget, get_values, bind_box, get_details, list_box, listboxin, update, update_input, RecentTransactions, DataInput
+from modules.exit import Exit
+
+
+FONT3 = ("Century Gothic", 8, "bold")
+
+
+class RecentEntries(RecentTransactions):
+    """
+    Displays listboxes that contain recent transactions for the ENTRY Tab
+    """
+
+    def __init__(self, frame:Frame):
+
+        self.frame = frame
+        self.file_name = "Entries"
+
+        super().__init__(self.frame, file=self.file_name)
+
+        self.des_label.config(text="[Recent ENTRIES]")
+
+
+class Input(DataInput):
+    """
+    - This class displays the Article and Article ID listboxes
+    - Also responsible for registering ENTRIES
+    """
+    def __init__(self, frame:Frame, updates:RecentEntries,  exit_up:Exit):
+
+        self.frame = frame
+        self.update = updates
+        self.exit_updates = exit_up
+
+        super().__init__(frame=self.frame, updates=self.update)
+
+        self.article_listbox = list_box(frame=self.frame, x_cor=10, y_cor=60, l_height=25, l_width=45)
+
+        self.id_listbox = list_box(frame=self.frame, x_cor=300, y_cor=60, l_height=25, l_width=30)
+
+        self.text_label.place(x=150, y=465)
+
+        self.select_btn.configure(width=80, command=self.selected)
+        self.select_btn.place(x=10, y=375)
+
+        self.article_entry_label.place(x=10, y=550)
+
+        self.article_entry_entry.place(x=10, y=465)
+
+        self.id_label.place(x=10, y=640)
+
+        self.id_entry.place(x=10, y=535)
+
+        self.article_unit_label.place(x=10, y=725)
+
+        self.unit_entry.place(x=10, y=600)
+
+        self.article_qty_label.place(x=10, y=810)
+
+        self.qty_entry.place(x=10, y=670)
+
+        self.cancel_btn.configure(hover_color="red", command=self.cancel_tran)
+        self.cancel_btn.place(x=350, y=560)
+
+        self.validate_btn.configure(text="Confirm Entry", hover_color="green", command=self.validate_entry)
+        self.validate_btn.place(x=350, y=640)
+
+        listboxin(self.article_listbox,  self.id_listbox)
+
+        bind_box(self.article_listbox,  self.id_listbox, func=self.mousewheel)
+
+
+    def mousewheel(self, event):
+        """This function causes the listbox to scroll together"""
+        self.article_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+        self.id_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+
+
+    def selected(self):
+        """
+        Uses the get_details function to auto insert the article name, ID, unit and display the current quantity
+        """
+        clear(self.article_entry_entry, self.id_entry, self.unit_entry, self.qty_entry)
+        try:
+            art_name, art_id, art_unit, art_qty = get_details(self.article_listbox)
+        except TypeError:
+            messagebox.showinfo(
+            title="Error",
+            message="No ARTICLE was selected\n--\nOnly select from the ARTICLE list"
+            )
+        else:
+
+            self.current_label.place(x=10, y=920)
+            self.current_qlabel.place(x=80, y=920)
+
+            self.article_entry_entry.insert(END, art_name[:-5])
+            self.id_entry.insert(END, art_id)
+            self.unit_entry.insert(END, art_unit)
+            self.current_qlabel.config(text=art_qty)
+
+    def cancel_tran(self):
+        """Clears all entries for the entry tab and also hides Current quantity Labels"""
+
+        clear(self.article_entry_entry, self.id_entry, self.unit_entry, self.qty_entry)
+        forget(self.current_label, self.current_qlabel)
+
+
+    def validate_entry(self):
+        """
+        - Gets all Article details and the date, ensure a descriptive Article name/ID, 
+        ensures that only integers are entered as quantity
+        - Appends the transaction data to the Entries, General ledger and 
+        Stock level csv files if all conditions have been fulfilled 
+        and the file exits otherwise, it creates the files and appends the data
+        - If the Article is already in stock and the Article to be stored has the same ARTICLEID 
+        as the Article in stock, it deletes the previous Article data 
+        and inserts a new data with an updated quantity 
+        (adds current quantity to new quantity)
+        - Uses the UPDATE and UPDATE_INPUT functionsto all the Article and Article ID on theExit and Entry Tabs
+        """
+
+        current_time = datetime.datetime.now()
+        date = current_time.strftime("%d-%b-%Y")
+
+        art_name, art_id, art_unit, art_qty, *_ = get_values(self.article_entry_entry, self.id_entry, self.unit_entry, self.qty_entry)
+        stock_name = f"{art_name.upper()}[{art_id[:3].title()}]"
+
+        if len(art_name) < 7 or len(art_id) < 4:
+            messagebox.showinfo(
+                title="Article Error",
+                message="Please enter a descriptive and simple Article name/ID that will distinguish it from similar materials\nAlso ensure the ARTICLEID is not empty"
+            )
+        elif len(art_unit) < 1 or not art_unit.isalpha():
+            messagebox.showinfo(
+                title="Modify Unit box",
+                message="Please enter a more descriptive Units to distinguish it from similar materials\nOnly use alphabetic characters"
+            )
+        elif not art_qty.isdecimal() or int(art_qty) < 1 :
+            messagebox.showinfo(
+                title="Invalid quantity",
+                message="Quantities must be numerical and cannot be less than 1"
+            )
+        else:
+
+            tran_validate = messagebox.askokcancel(
+                title="Confirm Entry",
+                message=f"Article: {art_name.upper()}\nArticleID: {art_id.title()}\nUnit: {art_unit}\nQuantity: {art_qty}"
+                )
+
+            if tran_validate is True:
+                new_data = {
+                    "Date" : [date],
+                    "Time" : [f"{current_time.strftime('%H:%M:%S')}"],
+                    "ArticleID": [art_id.title()],
+                    "Article" : [art_name.upper()],
+                    "Unit" : [art_unit.lower()],
+                    "Quantity" : [art_qty]
+                }
+                d_f = pandas.DataFrame(new_data)
+
+                stk_data = {
+                    "ArticleID": [art_id.title()],
+                    "Article" : [stock_name],
+                    "Unit" : [art_unit.lower()],
+                    "Quantity" : [art_qty]
+                }
+                new_stk_df = pandas.DataFrame(stk_data)
+
+                try:
+                    pandas.read_csv("../data/Entries.csv")
+                    pandas.read_csv("../data/General_ledger.csv")
+                    stock_data = pandas.read_csv("../data/Stock_level.csv")
+                except FileNotFoundError:
+                    d_f.to_csv("../data/Entries.csv", mode='a', index=False)
+                    d_f.to_csv("../data/General_ledger.csv", mode='a', index=False)
+                    new_stk_df.to_csv("../data/Stock_level.csv", mode='a', index=False)
+                    old_art_list = []
+                else:
+                    stk_df = pandas.DataFrame(stock_data)
+                    d_f.to_csv("../data/Entries.csv", mode='a', index=False, header=False)
+                    d_f.to_csv("../data/General_ledger.csv", mode='a', index=False, header=False)
+
+                    old_art_list = stock_data.Article.to_list()
+                    stock_lenght = len(stock_data.Article.to_list())
+
+                    for (i, row) in stk_df.iterrows():
+
+                        if row.Article == stock_name and row.ArticleID == art_id:
+
+                            stk_df = stk_df.drop(stk_df.index[i], axis=0)
+                            stk_df.to_csv("../data/Stock_level.csv", index=False)
+
+                            updated_data = {
+                                "ArticleID": [art_id.title()],
+                                "Article" : [stock_name],
+                                "Unit" : [art_unit.lower()],
+                                "Quantity" : [int(art_qty) + row.Quantity]
+                            }
+                            updated_stk_df = pandas.DataFrame(updated_data)
+                            updated_stk_df.to_csv("../data/Stock_level.csv", mode='a', index=False, header=False)
+                            stock_lenght -= 1
+
+                    #This line of code checks to see if a new record was created to avoid duplicating entries
+                    if stock_lenght == len(stock_data.Article.to_list()):
+                        new_stk_df.to_csv("../data/Stock_level.csv", mode='a', index=False, header=False)
+
+                clear(self.article_entry_entry, self.id_entry, self.unit_entry, self.qty_entry)
+
+
+                update(self.update.article_listbox, self.update.ID_listbox, self.update.date_listbox, self.update.quatity_listbox, file="Entries")
+
+                update_input(self.article_listbox, self.id_listbox, self.exit_updates.article_listbox, self.exit_updates.id_listbox, name=stock_name, old_data=old_art_list)
+
+                forget(self.current_label, self.current_qlabel)
---
+++ modules/functions.py
@@ -0,0 +1,309 @@
+"""This module contains functions and Classes that is repeatedly used by all the other scripts"""
+
+from tkinter import Frame, Listbox, Label, END
+import customtkinter
+import pandas
+
+FG = "white"
+BACKGROUND_COLOR = "#212121"
+SKY_BLUE = "#87CEEB"
+FONT1=("Century Gothic", 12, "bold")
+FONT2=("Century Gothic", 10, "bold")
+FONT3 = ("Century Gothic", 8, "bold")
+
+
+def clear(*args):
+    """Clears the text in any entry widget that is passed as an argument"""
+    for n in args:
+        n.delete(0, END)
+
+def forget(*args):
+    """automatically hides any widget"""
+    for n in args:
+        n.place_forget()
+
+def get_values(*args):
+    """
+    gets the values from any entry widget, stores it in a list 
+    and returns a tuple containing all the results that can easily be unpacked
+    """
+    result = []
+    for n in args:
+        result.append(n.get())
+    return tuple(result)
+
+
+def bind_box(*args, func):
+    """Binds mouse scroll to listboxes"""
+    for n in args:
+        n.bind("<MouseWheel>", func)
+
+
+def get_details(listbox):
+    """
+    :param listbox - Listbox to get Article metadata
+
+    Gets the Article, Article ID, Unit and Quantity of the selected material and returns them.
+    """
+
+    stock_data = pandas.read_csv("./data/Stock_level.csv")
+    selected = ""
+
+    for i in listbox.curselection():
+        selected = listbox.get(i)
+
+    for (_ , row) in stock_data.iterrows():
+
+        if row.Article[:-5] == selected:
+            return row.Article, row.ArticleID, row.Unit, row.Quantity
+    return None
+
+
+def list_box(frame:Frame, x_cor:int, y_cor:int, l_height:int, l_width:int):
+    """
+    :param frame - The frame the listbox will be created in
+    :param x_cor - The x coordinate of the listbox
+    :param y_cor - The y coordinate of the listbox
+    :param l_height - The desired lenght of the listbox
+    :param l_width - The desired width of the listbox
+
+
+    This function lets you create a listbox and also define the height and width you desire
+    """
+    listbox = Listbox(master=frame, height=l_height, width=l_width)
+    listbox.place(x=x_cor, y=y_cor)
+
+    return listbox
+
+
+def style_bg(box, length:int):
+    """
+    :param box - listbox to be modified
+    :param lenght - number of times to iterate
+    
+    Styles the background of a listbox
+    """
+    for line in box:
+        for i in range(0, length):
+            line.itemconfigure(i, bg="white")
+
+    for line in box:
+        for i in range(0, length, 2):
+            line.itemconfigure(i, bg=SKY_BLUE)
+
+
+def listboxin(*box):
+    """
+    :param *box - listbox to be modified
+
+    This function is responsible for inserting data into the Article and Article ID listbox
+    """
+
+    try:
+        stock_data = pandas.read_csv("./data/Stock_level.csv")
+    except FileNotFoundError:
+        print("No data")
+    else:
+
+        article_list = stock_data.Article.to_list()
+        article_id_list = stock_data.ArticleID.to_list()
+
+        rev_index = len(article_list)
+
+        if len(box)<2:
+            for item in article_list:
+                box[0].insert(0, item)
+        else:
+            for item in article_list:
+                box[0].insert(0, item[:-5])
+
+            for item in article_id_list:
+                box[1].insert(0, item)
+
+        style_bg(box, length=rev_index)
+
+
+def insert_info(*box, file):
+    """
+    :param *box - listbox to be modified
+    :param file - File to upload data that will be used by the listboxes
+
+    This function is responsible for inserting data into all Recent Transaction listbox
+    """
+    try:
+
+        data = pandas.read_csv(f"./data/{file}.csv")
+    except FileNotFoundError:
+        print("No data")
+    else:
+
+        entries_article_list = data.Article.to_list()
+        entries_article_id_list = data.ArticleID.to_list()
+        entries_date_list = data.Date.to_list()
+        entries_qty_list = data.Quantity.to_list()
+
+        rev_index = len(entries_date_list)
+
+        for item in entries_article_list:
+            box[0].insert(0, item)
+
+        for item in entries_article_id_list:
+            box[1].insert(0, item)
+
+        for item in entries_date_list:
+            box[2].insert(0, item)
+
+        for item in entries_qty_list:
+            box[3].insert(0, item)
+
+        style_bg(box, length=rev_index)
+
+
+def update(*box, file):
+
+    """
+    :param *box - listbox to be modified
+    :param file - File to upload data that will be used by the listboxes
+    This function is responsible for updating data Recent Transactions listboxes
+    """
+    try:
+
+        data = pandas.read_csv(f"./data/{file}.csv")
+    except FileNotFoundError:
+        print("No data")
+    else:
+
+        entries_article_list = data.Article.to_list()
+        entries_article_id_list = data.ArticleID.to_list()
+        entries_date_list = data.Date.to_list()
+        entries_qty_list = data.Quantity.to_list()
+        rec_rev_index = len(entries_article_list)
+
+        box[0].insert(0, entries_article_list[-1])
+        box[1].insert(0, entries_article_id_list[-1])
+        box[2].insert(0,  entries_date_list[-1])
+        box[3].insert(0,  entries_qty_list[-1])
+
+        style_bg(box, length=rec_rev_index)
+
+
+def update_input(*box, name:str, old_data:list):
+    """
+    :param *box - listbox to be updated
+    :param name - name of Article to be inserted
+    :param old_data - old Article List obtained from Stock_level before entry
+    
+    Updates the Article and ArticleID listboxes across all TABS
+    """
+
+    try:
+        stock_data = pandas.read_csv("./data/Stock_level.csv")
+    except FileNotFoundError:
+        print("No data")
+    else:
+
+        article_list = stock_data.Article.to_list()
+        article_id_list = stock_data.ArticleID.to_list()
+
+        if name not in old_data:
+            box[0].insert(0, article_list[-1][:-5])
+            box[1].insert(0, article_id_list[-1])
+            box[2].insert(0, article_list[-1][:-5])
+            box[3].insert(0, article_id_list[-1])
+
+        style_bg(box, length=len(article_list))
+
+
+class RecentTransactions():
+    """
+    This class responsible for creating listboxes that display recent transactions
+    """
+
+    def __init__(self, frame:Frame, file):
+
+        self.frame = frame
+        self.file_name = file
+
+        self.des_label = Label(master=self.frame, text="", font=FONT2, fg="red", bg=BACKGROUND_COLOR)
+        self.des_label.place(x=380, y=10)
+
+        self.article_label= Label(master=self.frame, text="Article", font=FONT3, bg=BACKGROUND_COLOR, fg=FG)
+        self.article_label.place(x=155, y=45)
+
+        self.id_label= Label(master=self.frame, text="Article ID", font=FONT3, bg=BACKGROUND_COLOR, fg=FG)
+        self.id_label.place(x=470, y=45)
+
+        self.date_label= Label(master=self.frame, text="Date", font=FONT3, bg=BACKGROUND_COLOR, fg=FG)
+        self.date_label.place(x=670, y=45)
+
+        self.qty_label= Label(master=self.frame, text="QTY", font=FONT3, bg=BACKGROUND_COLOR, fg=FG)
+        self.qty_label.place(x=760, y=45)
+
+        self.article_listbox = list_box(frame=self.frame, x_cor=75, y_cor=70, l_height=40, l_width=50)
+
+        self.ID_listbox = list_box(frame=self.frame, x_cor=380, y_cor=70, l_height=40, l_width=40)
+
+        self.date_listbox = list_box(frame=self.frame, x_cor=625, y_cor=70, l_height=40, l_width=20)
+
+        self.quatity_listbox = list_box(frame=self.frame, x_cor=750, y_cor=70, l_height=40, l_width=10)
+
+        bind_box(self.article_listbox, self.ID_listbox, self.date_listbox, self.quatity_listbox, func=self.mousewheel)
+        insert_info(self.article_listbox, self.ID_listbox, self.date_listbox, self.quatity_listbox, file=self.file_name)
+
+    def mousewheel(self, event):
+        """Responsible for binding all listboxes to mousewheel"""
+        self.article_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+        self.ID_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+        self.date_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+        self.quatity_listbox.yview_scroll(-2 * int(event.delta / 120), "units")
+
+
+
+class DataInput():
+
+    def __init__(self, frame:Frame, updates):
+
+        self.frame = frame
+        self.update = updates
+
+        self.article_listbox = ""
+
+        self.article_label = Label(master=self.frame, text="ARTICLE", font=FONT1, fg=FG, bg=BACKGROUND_COLOR)
+        self.article_label.place(x=120, y=35)
+
+        self.articleid_label = Label(master=self.frame, text="ARTICLE ID", font=FONT1, fg=FG, bg=BACKGROUND_COLOR)
+        self.articleid_label.place(x=350, y=35)
+
+        self.select_btn = customtkinter.CTkButton(master=self.frame, text="Select", font=FONT3)
+
+        self.text_label = Label(master=self.frame, text="[Select existing ARTICLE]", font=FONT3, fg="red", bg=BACKGROUND_COLOR)
+
+        self.article_entry_label= Label(master=self.frame, text="ARTICLE", font=FONT3, fg=FG, bg=BACKGROUND_COLOR)
+
+        self.article_entry_entry = customtkinter.CTkEntry(master=self.frame, fg_color="white", text_color="black", width=250)
+
+        self.id_label = Label(master=self.frame, text="ARTICLE ID", font=FONT3, fg=FG, bg=BACKGROUND_COLOR)
+
+        self.id_entry = customtkinter.CTkEntry(master=self.frame, fg_color="white", text_color="black", width=250)
+
+        self.article_unit_label = Label(master=self.frame, text="UNIT", font=FONT3, fg=FG, bg=BACKGROUND_COLOR)
+
+        self.unit_entry = customtkinter.CTkEntry(master=self.frame, fg_color="white", text_color="black", width=250)
+
+        self.article_qty_label = Label(master=self.frame, text="QUANTITY", font=FONT3, fg=FG, bg=BACKGROUND_COLOR)
+
+        self.qty_entry = customtkinter.CTkEntry(master=self.frame, fg_color="white", text_color="black", placeholder_text="Insert quantity", width=250)
+
+        self.current_label = Label(master=self.frame, text="Current Qty: ", font=FONT3, fg="red", bg=BACKGROUND_COLOR)
+
+        self.current_qlabel = Label(master=self.frame, text="we", font=FONT3, fg="red", bg=BACKGROUND_COLOR)
+
+        self.current_qty_entry = customtkinter.CTkEntry(master=self.frame, text_color="black", fg_color=FG, width=60)
+
+        self.exit_qty_label = Label(master=self.frame, text="EXIT QTY", font=FONT3, fg=FG, bg=BACKGROUND_COLOR)
+
+        self.exit_qty_entry = customtkinter.CTkEntry(master=self.frame, width=60, text_color="black", fg_color=FG, placeholder_text="Insert quantity")
+
+        self.cancel_btn = customtkinter.CTkButton(master=self.frame, text="Cancel transaction",  font=FONT3, width=180)
+
+        self.validate_btn= customtkinter.CTkButton(master=self.frame, text="Confirm", font=FONT1, width=180)
